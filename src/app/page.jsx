b7b15fb9fc8edc2eb0690c428284aee70d9908f9
NJ-NafsinJahan <nj.nafsinjahan@gmail.com>
2026-06-17 19:30:38 +0600

    Initial assignment starter code

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,20 @@
+import Hero from "@/components/Hero";
+import Statistics from "@/components/Statistics";
+import Testimonials from "@/components/Testimonials";
+import WhyChoose from "@/components/WhyChoose";
 
 export default async function HomePage() {
-
-
+  const stats = {
+    totalEvents: 30,
+    totalAttendees: 4000,
+    totalOrgs: 22,
+  };
   return (
     <div>
-      Welcome to Ticketo.
+      <Hero></Hero>
+      <WhyChoose></WhyChoose>
+      <Statistics stats={stats}></Statistics>
+      <Testimonials></Testimonials>
     </div>
   );
 }
-
